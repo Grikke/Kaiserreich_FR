6b96bd8584b1aebe80de9ffdd0c4db5fc03bd089
NicolasGrosjean <grosjen@hotmail.fr>
2018-04-27 23:05:38 +0200

    Add an option to export English files

--- TranslationExporter/src/main/TranslationExporter.java
+++ TranslationExporter/src/main/TranslationExporter.java
@@ -4,12 +4,14 @@ import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class TranslationExporter {
 	private static final String OUTPUT_DIR_NAME = "output";
+	private static boolean COPY_ENGLISH = false;
 	
 	
 	public static void main(String[] args) throws IOException {
@@ -17,9 +19,19 @@ public class TranslationExporter {
 		String subOutputDir = createNewDirectory();
 		
 		File localisationDir = new File("..");
+		// Copy English files and put l_french instead
+		if (COPY_ENGLISH)
+		{
+			for (File f : localisationDir.listFiles(new EnglishFileFilter()))
+			{
+				Files.copy(f.toPath(), new File(subOutputDir + "/" + f.getName().replace("l_english", "l_french")).toPath());
+			}
+		}
+		// Erase with the translated files
 		for (File f : localisationDir.listFiles(new FrenchFileFilter()))
 		{
-			Files.copy(f.toPath(), new File(subOutputDir + "/" + f.getName()).toPath());
+			Files.copy(f.toPath(), new File(subOutputDir + "/" + f.getName()).toPath(),
+					StandardCopyOption.REPLACE_EXISTING);
 		}
 	}
 	
@@ -58,6 +70,14 @@ public class TranslationExporter {
 		@Override
 		public boolean accept(File dir, String name) {
 			return name.endsWith("l_french.yml");
+		}		
+	}
+	
+	private static class EnglishFileFilter implements FilenameFilter
+	{
+		@Override
+		public boolean accept(File dir, String name) {
+			return name.endsWith("l_english.yml");
 		}		
 	}
 }
